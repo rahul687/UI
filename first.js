bf56dfa0802d908d9edbf3f1309e61b2bb2714b9
rahul687 <rahulverma8087@gmail.com>
2018-03-28 13:17:31 -0500

    Add files via upload

---
+++ first.js
@@ -0,0 +1,74 @@
+string
+number
+boolean
+
+var name="rahul"
+var name="rahul"
+undefined
+name
+"rahul"
+var a = "javascript"
+undefined
+a
+"javascript"
+
+var b = "10"
+var c = "true"
+
+var d = 10;
+var c = true;
+
+var a = "rahul"
+undefined
+typeof(a)
+"string"
+var b =10
+undefined
+typeof(b)
+"number"
+var c = true
+undefined
+typeof(c)
+"boolean"
+
+///////
+1+1
+2
+2-1
+1
+2*6
+12
+6/7
+0.8571428571428571
+
+
+
+
+undefined
+var b = 20
+undefined
+a+b
+30
+var a = "rhaul"
+undefined
+var b = "    javascript"
+undefined
+a+b
+"rhaul    javascript"
+var d = " age of aakash is"
+undefined
+var e = 10
+undefined
+d+e
+" age of aakash is10"
+10+10+"10"
+"2010"
+"10"+10+10
+"101010"
+10+"10"+10
+"101010"
+
+
+
+
+
