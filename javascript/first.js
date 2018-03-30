38a5e0a21d47104993b3745d15e22ee3323c1a8c
rahul687 <rahulverma8087@gmail.com>
2018-03-30 08:59:27 -0500

    Js

--- javascript/first.js
+++ javascript/first.js
@@ -68,7 +68,93 @@ d+e
 10+"10"+10
 "101010"
 
+/////////array/////
+var a = [1,2,3,4,5,6,7]
+var b = ["a","b","c"]
+var c = [1,2,"a","rahul",3,4,"a",6,true,6,"a",1,2,3]
 
+var a = [1,2,3,4,5,6,7]
+undefined
+a.length
+7
+a[7]
+undefined
+a[6]
+7
+a[1]
+2
+var a = [1,2,3,4,5,6,7]
+undefined
+a.length
+7
+a.push("a")
+8
+a
+(8) [1, 2, 3, 4, 5, 6, 7, "a"]
+a.push("rahul")
+9
+a
+(9) [1, 2, 3, 4, 5, 6, 7, "a", "rahul"]
+a.length
+9
+a.pop(2)
+"rahul"
+a
+(8) [1, 2, 3, 4, 5, 6, 7, "a"]
+a.pop()
+"a"
+a
+(7) [1, 2, 3, 4, 5, 6, 7]
+a.unshift("apple")
+8
+a
+(8) ["apple", 1, 2, 3, 4, 5, 6, 7]
+a.unshift("mango")
+9
+a
+(9) ["mango", "apple", 1, 2, 3, 4, 5, 6, 7]
+a.shift(1)
+"mango"
 
+var a = [1,2,3,4,5,6,7]
 
+a.slice(2,4)
+(2) [3, 4]
+a
+(7) [1, 2, 3, 4, 5, 6, 7]
+a.splice(2,0,"apple","mango")
+[]
+a
+(9) [1, 2, "apple", "mango", 3, 4, 5, 6, 7]
+a.splice(2,2,"bmw","audi")
+(2) ["apple", "mango"]
+a
+(9) [1, 2, "bmw", "audi", 3, 4, 5, 6, 7]
+a.splice(4,1,"red","yellow")
+[3]
+a
+(10) [1, 2, "bmw", "audi", "red", "yellow", 4, 5, 6, 7]
 
+(10) [1, 2, "bmw", "audi", "red", "yellow", 4, 5, 6, 7]
+a.indexOf(4)
+6
+a
+(10) [1, 2, "bmw", "audi", "red", "yellow", 4, 5, 6, 7]
+a.indexOf("rahul")
+-1
+var a=["apple","mango"]
+
+undefined
+var b =[1,2]
+undefined
+a+b
+"apple,mango1,2"
+a.concat(b)
+(4) ["apple", "mango", 1, 2]
+var c= ["bmw","audi"]
+        
+undefined
+a.concat(b,c)
+(6) ["apple", "mango", 1, 2, "bmw", "audi"]
+b.concat(a,c)
+(6) [1, 2, "apple", "mango", "bmw", "audi"]
---
+++ javascript/rahul.js
@@ -0,0 +1,56 @@
+var c = 10
+undefined
+var e ="cool"
+undefined
+c+e
+"10cool"
+var r = "10"
+undefined
+
+var d = cool
+VM958:1 Uncaught ReferenceError: cool is not defined
+    at <anonymous>:1:9
+(anonymous) @ VM958:1
+
+var d =10
+undefined
+
+d+r
+"1010"
+d+c
+20
+
+d+e
+"10cool"
+
+10+10+"10
+
+VM990:1 Uncaught SyntaxError: Invalid or unexpected token
+
+10+10+"10"
+"2010"
+
+35-23+939+"rahul"
+"951rahul"
+
+var t = " javascript need logic"
+undefined
+
+var a ="need to work hard for this "
+undefined
+
+var f = 2018 "year"
+VM1020:1 Uncaught SyntaxError: Unexpected string
+
+var f = 2018 +2019
+undefined
+
+t+a+f
+" javascript need logicneed to work hard for this 4037"
+
+var t = " javascript need logic "
+undefined
+
+t + a 
+" javascript need logic need to work hard for this "
+
