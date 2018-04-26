f35ad8af2d1e5d15e8327171758d88d2ef46f146
rahul687 <rahulverma8087@gmail.com>
2018-04-25 20:31:42 -0500

    add jquery

--- jquery/jqueryJs2.js
+++ jquery/jqueryJs2.js
@@ -3,3 +3,21 @@ function doIt(){
     toDos.find('.js-complete').addClass('blue')
     toDos.parent().removeClass('hidden')
 }
+
+function milk(){
+    $('.milk').addClass('check')
+}
+
+function rent(){
+    $('.rent').addClass('check')
+}
+
+function party(){
+    $('.party').addClass('check')
+}
+
+function reset(){
+    $('.milk').removeClass('check')
+    $('.rent').removeClass('check')
+    $('.party').removeClass('check')
+}
